35f69f4ca75429bd75fb54b1a2dd0b4945c5a0c2
Kyle Killian <61425924+diglettusedswim@users.noreply.github.com>
2020-09-12 21:30:23 -0600

    Create database.php

---
+++ database.php
@@ -0,0 +1,13 @@
+<?php
+    $dsn = 'mysql:host=localhost;dbname=portfolio';
+    $username = 'recrute_user';
+    $password = 'Pa$$w0rd';
+
+    try {
+        $db = new PDO($dsn, $username, $password);
+    } catch (PDOException $e) {
+        $error_message = $e->getMessage();
+        include('database_error.php');
+        exit();
+    }
+?>
