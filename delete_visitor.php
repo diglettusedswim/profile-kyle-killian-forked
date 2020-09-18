2f8408833da1862daf957cb3ab6798dd470185c1
Kyle Killian <61425924+diglettusedswim@users.noreply.github.com>
2020-09-18 15:57:13 -0600

    Update delete_visitor.php

--- delete_visitor.php
+++ delete_visitor.php
@@ -1,6 +1,9 @@
+<!-------MOD LOG-------------------------------
+---------DATE: 9/18/2020-----------------------
+---------Made delete_visitor.php-------------->
 <?php
-require_once('database.php');
-
+require_once('./database/database.php');
+require_once('./database/visitor.php');
 // Get IDs
 $visitor_id = filter_input(INPUT_POST, 'visitor_id', FILTER_VALIDATE_INT);
 $user_id = filter_input(INPUT_POST, 'userID', FILTER_VALIDATE_INT);
@@ -11,13 +14,14 @@ $user_id = filter_input(INPUT_POST, 'userID', FILTER_VALIDATE_INT);
 
 // Delete the product from the database
 if ($visitor_id != false && $user_id != false) {
-    $query = 'DELETE FROM visitor
-              WHERE visitor_id = :visitor_id';
-//    echo "this works";
-    $statement = $db->prepare($query);
-    $statement->bindValue(':visitor_id', $visitor_id);
-    $success = $statement->execute();
-    $statement->closeCursor();    
+//    $query = 'DELETE FROM visitor
+//              WHERE visitor_id = :visitor_id';
+////    echo "this works";
+//    $statement = $db->prepare($query);
+//    $statement->bindValue(':visitor_id', $visitor_id);
+//    $success = $statement->execute();
+//    $statement->closeCursor();   
+    delVisitor($visitor_id);
 }
 
 // Display the Product List page
