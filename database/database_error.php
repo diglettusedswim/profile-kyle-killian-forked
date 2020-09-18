65df4307b2d03d8635f7c70fa17ed0ae55b2ee1c
Kyle Killian <61425924+diglettusedswim@users.noreply.github.com>
2020-09-18 15:57:04 -0600

    Create database_error.php

---
+++ database/database_error.php
@@ -0,0 +1,64 @@
+<!-------MOD LOG-------------------------------
+---------DATE: 9/18/2020-----------------------
+---------Made Database.php---------------------
+---------moved code from from source files-----
+---------to here------------------------------>
+<?php
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="description" content="My personal portfolio website" />
+        <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="./favicon/digglett.png"
+            />
+        <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="./favicon/digglett.png"
+            />
+        <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="./favicon/digglett.png"
+            />
+        <link rel="manifest" href="./favicon/site.webmanifest" />
+        <title>Kyle Killian</title>
+        <link href="../style.css" rel="stylesheet" type="text/css"/>
+    </head>
+    <body>
+        <nav id="nav" class="hideNav">
+            <ul class="navbar">
+                <li><a href="../index.html#home">Home</a></li>
+                <li><a href="../index.html#projects">Projects</a></li>
+                <li><a href="../index.html#about">About</a></li>
+                <li><a href="../login.php">Admin</a></li>
+            </ul>
+        </nav>
+        <div id="home">
+            <div class="showcase">
+                <div class="container">
+                    <h1 id="name">Error!:
+                        <span                           
+                            class="txt-type"
+                            data-wait="1500"
+                            data-words='["There were an error!"]'
+                            >
+                        </span> 
+                    </h1>
+                    <a href="../index.html" target="_top">Click me</a>
+                </div>
+            </div>
+        </div>
+    </body>
+    <script src="main.js"></script>
+</html>
