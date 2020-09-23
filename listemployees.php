1272b68ffceed195d01326ac29e39d10deba3ac7
Kyle Killian <61425924+diglettusedswim@users.noreply.github.com>
2020-09-23 15:31:42 -0600

    Update listemployees.php

--- listemployees.php
+++ listemployees.php
@@ -5,34 +5,7 @@
 /* * *************************************** */
 /* * *************************************** */
 
-class Database {
 
-    private static $dsn = 'mysql:host=localhost;dbname=portfolio';
-    private static $username = 'recrute_user';
-    private static $password = 'Pa$$w0rd';
-    private static $db;
-
-    private function __construct() {
-        
-    }
-
-    public static function getDB() {
-        if (!isset(self::$db)) {
-            try {
-                self::$db = new PDO(self::$dsn,
-                        self::$username,
-                        self::$password);
-            } catch (PDOException $e) {
-                $error_message = $e->getMessage();
-                //include('../errors/database_error.php');
-                echo "Connection error";
-                exit();
-            }
-        }
-        return self::$db;
-    }
-
-}
 
 class Employee {
 
