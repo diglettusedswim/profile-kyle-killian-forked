ed83efdfd9ef0d826d852c3be037e5f8ac843c79
Kyle Killian <61425924+diglettusedswim@users.noreply.github.com>
2020-09-23 15:31:36 -0600

    Update employee.php

--- database/employee.php
+++ database/employee.php
@@ -1,19 +1,24 @@
 <!-------MOD LOG-------------------------------
 ---------DATE: 9/18/2020-----------------------
 ---------moved over getEmployees---------------
----------as a function------------------------->
+---------as a function-------------------------
+---------Date 9/23/2020------------------------
+---------Changed $db to use function----------->
 
 <?php
 
 function getEmployees() {
-    global $db;
-    $query = 'SELECT * FROM information ORDER BY userID';
-    $statement = $db->prepare($query);
-    $statement->execute();
-    $users = $statement->fetchAll();
-    $statement->closeCursor();
-    
+    try {
+        $db = Database::getDB();
+        $query = 'SELECT * FROM information ORDER BY userID';
+        $statement = $db->prepare($query);
+        $statement->execute();
+        $users = $statement->fetchAll();
+        $statement->closeCursor();
+    } catch (PDOException $e) {
+        include ('../database/database_error.php');
+    }
+
     return $users;
 }
-
 ?>
